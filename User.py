6fb961dd26a530eabf1d046d5bcfbd04e6aaf491
KerbezOrazgaliyeva <160107140@stu.sdu.edu.kz>
2019-03-21 21:40:07 +0600

    start, set_username, ishere commands added

--- User.py
+++ User.py
@@ -17,13 +17,30 @@ class User:
         self.realUsername = realUsername
         self.chatId = chatId
         ref = db.reference('users')
-        new_user_ref = ref.push({
-            'realUsername': realUsername,
-            'chatId': chatId,
-        })
-        self.id = new_user_ref.key
+        res = ref.get()
+        is_ok = 0
+        for id in res:
+            if res[id]['realUsername'] == realUsername:
+                self.id = id
+                self.fakeUsername = ref.child(id).child('fakeUsername').get()
+                self.defaultUser = ref.child(id).child('defaultUser').get()
+                print(self.fakeUsername, self.defaultUser)
+                is_ok = 1
+
+        if is_ok == 0:
+            new_user_ref = ref.push({
+                'realUsername': realUsername,
+                'chatId': chatId,
+                'fakeUsername': '-',
+                'defaultUser': '-',
+            })
+            self.id = new_user_ref.key
 
     def setFakeUsername(self, fakeUsername):
+        ref = db.reference('users')
+        ref.child(self.id).update({
+            'fakeUsername': fakeUsername
+        })
         self.fakeUsername = fakeUsername
 
     def setdefaultUser(self, defaultUser):
--- main.py
+++ main.py
@@ -4,20 +4,14 @@ import telebot
 import constants
 from User import User
 
-# cred = credentials.Certificate("anonturingbot.json")
-# firebase_admin.initialize_app('anonturingbot', cred, {
-#     'databaseURL': 'https://anonturingbot.firebaseio.com'})
-# firebase_admin.get_app()
-
 if 'anonturingbot' not in firebase_admin._apps:
     cred = credentials.Certificate('anonturingbot.json')
     firebase_admin.initialize_app(cred, {
             'databaseURL': 'https://anonturingbot.firebaseio.com',
             'storageBucket': 'anonturingbot.appspot.com'}, name='anonturingbot')
 
-
-
 bot = telebot.TeleBot(constants.token)
+
 
 @bot.message_handler(commands=['start'])
 def handle_start(message):
@@ -26,11 +20,53 @@ def handle_start(message):
     user = User(realUsername, chatId)
 
     user_res = db.reference('users').child(user.id).get()
-    print(user_res)
+    print(user_res['realUsername'])
+    print(user_res['fakeUsername'])
     if user_res is not None:
-        bot.send_message(user.chatId, 'it works')
+        if user.fakeUsername == '-':
+            bot.send_message(user.chatId, 'If you want to chat with other users, you need to create fake username \nWhite your fake username followed by /set_username command')
+        else:
+            bot.send_message(user.chatId, 'hi ' + user.realUsername)
     else:
         bot.send_message(message.chat.id, 'doesnt')
+
+
+@bot.message_handler(commands=['set_username'])
+def handle_start(message):
+    chatId = message.chat.id
+    realUsername = message.from_user.username
+    user = User(realUsername, chatId)
+    print(len(message.text.split()) )
+    if len(message.text.split()) == 2:
+        print('ok')
+        m = message.text.split()[1]
+        print(m)
+        user.setFakeUsername(m)
+        fakeUsername = user.fakeUsername
+        print(fakeUsername)
+    else:
+        bot.send_message(user.chatId, 'incorrect command format')
+
+
+@bot.message_handler(commands=['ishere'])
+def handle_start(message):
+    chatId = message.chat.id
+    realUsername = message.from_user.username
+    user = User(realUsername, chatId)
+    print(len(message.text.split()) )
+    if len(message.text.split()) == 2:
+        print('ok')
+        m = message.text.split()[1]
+        user_ref = db.reference('users').get()
+        is_ok = 0
+        for id in user_ref:
+            if user_ref[id]['realUsername'] == m:
+                bot.send_message(user.chatId, 'yes')
+                is_ok = 1
+        if is_ok == 0:
+            bot.send_message(user.chatId, 'no(')
+    else:
+        bot.send_message(user.chatId, 'incorrect command format')
 
 
 bot.polling(none_stop=True, interval=0)
